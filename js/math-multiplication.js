cce22d3347ceafae7f78249365fb0c4604b3837a
EvgeniUPS <evgeniups@gmail.com>
2021-11-21 23:18:34 +0200

    ...

--- js/math-multiplication.js
+++ js/math-multiplication.js
@@ -30,35 +30,75 @@ function nav() {
   })
 }
 
-function checkAnswer() {
+function createExample(min, max) {
+  console.log(99999999999)
   const num1 = document.querySelector('.item-training__num1')
-  num1.innerHTML = randomInteger(1, 9)
+  num1.innerHTML = randomInteger(min, max)
   const num2 = document.querySelector('.item-training__num2')
-  num2.innerHTML = randomInteger(1, 9)
+  num2.innerHTML = randomInteger(min, max)
   const sign = document.querySelector('.item-training__sign')
-  const answer = document.querySelector('.item-training__answer')
-
-  const answerBlock = document.querySelector('.answer-training')
-
   console.log(`num1.value`, +num1.innerHTML)
   console.log(`num2.value`, +num2.innerHTML)
-
   const rightAnswer = num1.innerHTML * num2.innerHTML
   console.log(`rightAnswer`, rightAnswer)
+  return rightAnswer
+}
+
+function createAnswerBlock(rightAnswer) {
+  const answerBlock = document.querySelector('.answer-training')
+  answerBlock.innerHTML = ''
+  const arrAnswer = [
+    randomInteger(1, 99),
+    randomInteger(1, 99),
+    randomInteger(1, 99),
+    randomInteger(1, 99),
+    rightAnswer,
+  ].sort(() => Math.random() - 0.5)
+  console.log(`arrAnswer`, arrAnswer)
+
+  arrAnswer.forEach(el => {
+    const answerItem = `
+    <div class="answer-training__item">${el}</div>
+    `
+    answerBlock.insertAdjacentHTML('afterbegin', answerItem)
+  })
+}
+const trainingStartBlock = document.querySelector('.training-start')
+const newGameBtn = document.querySelector('.training-start__button')
+
+function checkAnswer(rightAnswer) {
+  const answer = document.querySelector('.item-training__answer')
+  const answerBlock = document.querySelector('.answer-training')
 
   answerBlock.addEventListener('click', function (e) {
     if (e.target.classList.contains('answer-training__item')) {
       const selectedAnswer = +e.target.innerHTML
       console.log(`selectedAnswer`, selectedAnswer)
       if (selectedAnswer === rightAnswer) {
+        document.getElementById('errorSound').play()
         answer.innerHTML = rightAnswer
+        e.target.classList.add('right-answer')
+        trainingStartBlock.classList.add('_active')
+        newGameBtn.addEventListener('click', function (e) {
+          newGame()
+        })
+      } else {
+        e.target.classList.add('wrong-answer')
       }
     }
   })
 }
+const rightAnswer = createExample(1, 3)
+
+function newGame() {
+  trainingStartBlock.classList.remove('_active')
+}
 
 window.addEventListener('DOMContentLoaded', () => {
-  nav(), console.log(randomInteger(1, 9)), checkAnswer()
+  nav(),
+    console.log(randomInteger(1, 9)),
+    createAnswerBlock(rightAnswer),
+    checkAnswer(rightAnswer)
 })
 
 function randomInteger(min, max) {
@@ -68,3 +108,14 @@ function randomInteger(min, max) {
 }
 
 console.log(randomInteger(1, 9))
+// function randomArrayAnswer(params) {
+//   const arr = [
+//     randomInteger(1, 99),
+//     randomInteger(1, 99),
+//     randomInteger(1, 99),
+//     randomInteger(1, 99),
+//     555,
+//   ].sort(() => Math.random() - 0.5)
+//   return arr
+// }
+// console.log(`randomArrayAnswer`, randomArrayAnswer())
